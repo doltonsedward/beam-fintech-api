c9746dfe7252fb49a6826d70649c8367c1083edf
D3NCyan <edwarddoltons@gmail.com>
2022-01-05 11:24:26 +0800

    use express.json() and add api router

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 
-// app.use('api/v1')
+const app = express();
+const router = require("./src/routes");
 const port = process.env.PORT || 7500;
+
+app.use(express.json());
+
+app.use("/api/v1", router);
 
 app.listen(port, () => console.log(`Server running in port: ${port}`));
